b01cc50850914fd130be956141abb159a01e45a4
Brian Bradley <brianbradley@brians-air.attlocal.net>
2016-03-23 15:51:24 -0500

    Initial Icons Load on Map

--- js/app.js
+++ js/app.js
@@ -1,22 +1,66 @@
 // Map Key AIzaSyBO7zaTpALyiQJOvnvA7_muATPumXQT2uU
+//GeoSync Key  AIzaSyAB7BN8tkg05jkP4fsGts_jxQw_EPPrEW0
+     //Global map variable
 
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+
+var ViewModel = function() {
+    var self = this;
+    this.markerList = ko.observableArray([]);
+
+        };
+
+
+
+var siteAddress = [
+  {
+    name: "The Back Porch Grill",
+   address: "270 Marina Drive, Talladega, AL 35160"
+  },
+  {
+    name: "Chilly Williys",
+    address: "4300 Martin St S, Cropwell, AL 35054"
+
+   },
+   {
+    name: "Pam and Mike's",
+    address: "780 River Oaks Drive, Cropwell, Al"
+   }
+];
+
+
+var initMap = function() {
+    var map;
+    var elevator;
+    var myOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng(33.531254, -86.290179),
+        mapTypeId: 'terrain'
+    };
+    map = new google.maps.Map($('#map')[0], myOptions);
+
+    siteAddress.forEach(function(marker) {
+        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=' + marker.address + "&key=AIzaSyAB7BN8tkg05jkP4fsGts_jxQw_EPPrEW0",
+         function (data) {
+            var p = data.results[0].geometry.location
+            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            new google.maps.Marker({
+                position: latlng,
+                map: map,
+                icon: "img/boating.svg"
+            });
+
         });
-      }
+    })
+
+};
 
 
 
+ko.applyBindings(new ViewModel());
 
 
 
-
-
-
-
+//"https://maps.googleapis.com/maps/api/geocode/json?address=" + places.address + "&key=AIzaSyAB7BN8tkg05jkP4fsGts_jxQw_EPPrEW0",
 
 
 
